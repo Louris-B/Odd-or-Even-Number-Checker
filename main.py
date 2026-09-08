70fc2682c5da072a0b9189e1c569ff899ef67149
Louris-B <louris.borm@gmail.com>
2026-09-08 15:32:59 +0800

    Added odd or even number checker.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 num = int(input("Enter a positive whole number: ")) #Asks the user for the value of num
 
-if num < 0: #Checks whether num is a positive whole number
+if num < 0: #Checks whether num is a positive whole number 
     print("The value must be positive, please try again.") #Tells the user to retry
 else:
     if num % 2 == 0: #Checks whether num is even
